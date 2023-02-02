fc8efea466095c95ad1a80c397a58f29aa1afe29
a1740942002 <a1740942002@me.com>
2023-02-02 11:57:57 +0800

    add config

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,42 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import { Button } from './components/Button'
 
+interface Config {
+  style: 'vertical' | 'horizontal'
+  defaultChain: string
+  chains: any[]
+  theme: {
+    accentColor: string
+    secondaryColor: string
+    darkMode: true
+  }
+}
+
+const initialConfig: Config = {
+  style: 'vertical',
+  defaultChain: '5',
+  chains: [],
+  theme: {
+    accentColor: '#f0f0f0',
+    secondaryColor: 'f0f0f0',
+    darkMode: true
+  }
+}
+
 function App() {
   const [count, setCount] = useState(0)
 
+  const [config, setConfig] = useState<Config>(initialConfig)
+
   return (
     <div className="App">
-      <Button>Hello World</Button>
+      <XSwapWidget config={config} />
     </div>
   )
 }
 
 export default App
+
+function XSwapWidget(props: any) {
+  return <h1>X Swap</h1>
+}
